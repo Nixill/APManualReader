b3741b620bd1adb31c0b66182d49310dfc108aab
Nixill <github@nixill.net>
2026-05-05 17:00:27 -0400

    Ok, this seems to work.

---
+++ APManualReader/Program.cs
@@ -0,0 +1,73 @@
+using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using MudBlazor.Services;
+using APManualReader.Components;
+using Photino.Blazor;
+
+namespace APManualReader;
+
+public class Program
+{
+  [STAThread]
+  public static void Main(string[] args)
+  {
+    PhotinoBlazorAppBuilder appBuilder = PhotinoBlazorAppBuilder.CreateDefault(args);
+
+    ConfigureServices(appBuilder.Services);
+
+    appBuilder.RootComponents.Add<App>("app");
+
+    PhotinoBlazorApp app = appBuilder.Build();
+
+    var iconPath = ExtractEmbeddedResourceToTempFile("favicon.ico")!;
+
+    app.MainWindow
+        .SetSize(1400, 800)
+        .SetLogVerbosity(0)
+        .SetIconFile(iconPath)
+        .SetTitle("Photino.Blazor Embedded MudBlazor Sample");
+
+    AppDomain.CurrentDomain.UnhandledException += (_, error) =>
+    {
+      app.MainWindow.ShowMessage("Fatal exception", error.ExceptionObject.ToString()!);
+    };
+
+    app.Run();
+  }
+
+  private static void ConfigureServices(IServiceCollection services)
+  {
+    services.AddLogging();
+    services.AddSingleton<IFileProvider>(_ => new ManifestEmbeddedFileProvider(typeof(Program).Assembly, "wwwroot"));
+    services.AddMudServices();
+  }
+
+  private static string? ExtractEmbeddedResourceToTempFile(string fileName)
+  {
+    string resourceNamespace = typeof(Program).Namespace!;
+    string resourceName = $"{resourceNamespace}.wwwroot.{fileName}";
+
+    Assembly assembly = Assembly.GetExecutingAssembly();
+
+    using (var resourceStream = assembly.GetManifestResourceStream(resourceName))
+    {
+      if (resourceStream == null)
+      {
+        Console.WriteLine($"Resource {resourceName} not found.");
+        return null;
+      }
+
+      string tempFile = Path.Combine(Path.GetTempPath(), fileName);
+
+      using (FileStream fileStream = new FileStream(tempFile, FileMode.Create, FileAccess.Write))
+      {
+        resourceStream.CopyTo(fileStream);
+      }
+
+      return tempFile;
+    }
+  }
+}
